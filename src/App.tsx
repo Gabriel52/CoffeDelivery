17bb9323d2ee34d399804789ff8599b446683c99
Gabriel52 <gabrielbrune52@gmail.com>
2023-04-11 16:55:52 -0300

    fix: adjusting eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { RouterProvider } from 'react-router-dom'
-import { toast, ToastContainer } from 'react-toastify';
+import { RouterProvider } from 'react-router-dom';
+import {  ToastContainer } from 'react-toastify';
 
-import { Navbar } from './components/Navbar'
-import { ProductsProvider } from './Provider'
-import 'react-toastify/dist/ReactToastify.css';
+import { Navbar } from './components/Navbar';
+import { ProductsProvider } from './Provider';
+import "react-toastify/dist/ReactToastify.css";
 
-import { router } from './routes'
+import { router } from './routes';
 
 function App() {
   return (
@@ -27,7 +27,7 @@ function App() {
         <RouterProvider router={router} />
       </ProductsProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
